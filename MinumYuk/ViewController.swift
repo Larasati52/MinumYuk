102f4242f5540fa47fe9454959296642ad17e6ca
larasati <larasatinotodiharjo52@gmail.com>
2023-07-08 13:53:36 +0700

    Initial Commit

---
+++ MinumYuk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinumYuk
+//
+//  Created by Christophorus Davin on 08/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
